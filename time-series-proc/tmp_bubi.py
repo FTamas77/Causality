a6fe016c1564b15bbe27e028805cf20a6f130e96
Tamas <fekete331@gmail.com>
2024-07-09 10:15:11 +0200

    causal impact works

--- time-series-proc/tmp_bubi.py
+++ time-series-proc/tmp_bubi.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import pandas as pd
 from matplotlib import pyplot as plt
 
+import causalimpact
+
 ROOT_DIR = Path(os.path.dirname(os.path.abspath(__file__))).parent
 INPUT_DATA_FILE = os.path.join(
     ROOT_DIR, "datasets", "bubi", "bubi-weather_export_2212-2312.csv"
@@ -23,10 +25,8 @@ def read_and_filter_dataset(selected_parameters, input_data_file=INPUT_DATA_FILE
 
 
 def filter_groups(bubi, selected_parameters):
-    # bubi_intervention -> filter on station_name
-    # bubi_control -> filter on station_name
-    # keep all selected parameters
-    # keep ts_0 for resampling (start of the interval)
+    # bubi_intervention -> filter on station_name + add selected parameters + add ts_0
+    # bubi_control -> filter on station_name + add selected parameters + add ts_0
 
     def filter_by_station(station_prefix):
         return bubi.loc[
@@ -54,13 +54,13 @@ def resample_data(bubi_intervention, bubi_control):
     bubi_control_resampled = resample(bubi_control)
     print(bubi_control_resampled)
 
-    # from this point we don't need ts_0 and station_name
     return bubi_intervention_resampled, bubi_control_resampled
 
 
-def filter_by_date(bubi_intervention, bubi_control):
+def filter_by_date(bubi_intervention, bubi_control, start_date, end_date):
     def filter_df(df):
-        filtered_df = df.query("ts_0 >= '2023-05-01' and ts_0 < '2023-08-01'")
+        query_string = f"ts_0 >= '{start_date}' and ts_0 < '{end_date}'"
+        filtered_df = df.query(query_string)
         print(filtered_df)
         return filtered_df
 
@@ -73,17 +73,17 @@ def filter_by_date(bubi_intervention, bubi_control):
     return bubi_intervention_filtered, bubi_control_filtered
 
 
-def plot_data(intervention_data, control_data):
+def plot_data(intervention_data, control_data, intervention_start_date):
     ax = intervention_data.plot(color="blue", label="Intervention")
     control_data.plot(ax=ax, linestyle="dotted", color="green", label="Control")
 
     plt.axvline(
-        x=pd.to_datetime("2023-06-15"),
+        x=pd.to_datetime(intervention_start_date),
         color="red",
         linestyle=":",
         alpha=0.5,
         linewidth=2,
-        label="Key Date",
+        label="Intervention Start Date",
     )
 
     plt.title("Intervention vs. Control Group Over Time")
@@ -94,6 +94,26 @@ def plot_data(intervention_data, control_data):
     plt.legend(loc="upper right", edgecolor="red", shadow=True, title="Group")
 
     plt.show()
+
+
+def prepare_data(bubi_intervention, bubi_control, index_column, interest_column):
+    if bubi_intervention.index.name != index_column:
+        if index_column in bubi_intervention.columns:
+            bubi_intervention.set_index(index_column, inplace=True)
+        else:
+            raise KeyError(f"{index_column} is not in bubi_intervention columns")
+
+    if bubi_control.index.name != index_column:
+        if index_column in bubi_control.columns:
+            bubi_control.set_index(index_column, inplace=True)
+        else:
+            raise KeyError(f"{index_column} is not in bubi_control columns")
+
+    data = pd.DataFrame(
+        {"y": bubi_intervention[interest_column], "x1": bubi_control[interest_column]}
+    )
+
+    return data
 
 
 if __name__ == "__main__":
@@ -116,8 +136,32 @@ if __name__ == "__main__":
 
     bubi_intervention, bubi_control = resample_data(bubi_intervention, bubi_control)
 
-    bubi_intervention, bubi_control = filter_by_date(bubi_intervention, bubi_control)
+    # it is not needed
+    # I used it to have data around the intervention date
+    bubi_intervention_filtered, bubi_control_filtered = filter_by_date(
+        bubi_intervention, bubi_control, "2023-05-01", "2023-08-01"
+    )
 
-    plot_data(bubi_intervention, bubi_control)
+    # plot_data(bubi_intervention, bubi_control, "2023-06-15")
+
+    # prepare data for causal impact
+    index_column = "ts_0"
+    interest_column = "end_trip_no"
+    prepared_data = prepare_data(
+        bubi_intervention, bubi_control, index_column, interest_column
+    )
+    print(prepared_data.head())
+
+    pre_period = ("2023-05-01", "2023-06-15")
+    post_period = ("2023-06-16", "2023-09-01")
+
+    # run causal impact
+    impact = causalimpact.fit_causalimpact(
+        data=prepared_data, pre_period=pre_period, post_period=post_period
+    )
+
+    print(causalimpact.plot(impact))
+
+    print(causalimpact.summary(impact, output_format="summary"))
 
     print("end")
